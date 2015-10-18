bc719c6ee8f288547dd89a8637a06a7349c2ada1
Sammy <sammyimhook@gmail.com>
2015-10-18 19:27:19 +0100

    Add some fields and a spinner into the add noti screen

--- app/src/main/java/com/sammy/iscool/notificationapp/AddNewNotification.java
+++ app/src/main/java/com/sammy/iscool/notificationapp/AddNewNotification.java
@@ -2,6 +2,8 @@ package com.sammy.iscool.notificationapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddNewNotification extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class AddNewNotification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_notification);
+
+        Spinner spinner = (Spinner) findViewById(R.id.repeatSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.repeat_every_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
+
+
 }
